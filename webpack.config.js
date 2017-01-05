fdddfc9ad82cd2ca45a5e5c4d7ca6f642e610344
Inokentii Duka <Inokentii_Duka@epam.com>
2017-01-05 13:36:01 +0200

    scripts aded

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: './dist',
-        filename: 'bundle.js'
-    },
+  entry: './src/app.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
 
-            {
-                test: /\.css$/,
-                loader: 'css-loader'
-            }
-        ]
-    }
+      {
+        test: /\.css$/,
+        loader: 'css-loader'
+      }
+    ]
+  }
 };
